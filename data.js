83be38e0ffeedfa846462b77623539fa76a04ecb
Keren-nivea <153603635+Keren-nivea@users.noreply.github.com>
2024-12-16 21:55:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'frozen',
+        des: "Fearless optimist Anna teams up with rugged mountain man Kristoff and his loyal reindeer Sven in an epic journey to find Anna's sister Elsa, whose icy powers have trapped the kingdom of Arendelle in eternal winter.",
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'lubber pandhu',
+        des: "Set against the rural backdrop, explores gully cricket stars Gethu and Anbu's intense rivalry. Anbu's romantic involvement with Gethu's daughter escalates the clash. Their skills and emotions are put to the ultimate test.",
+        image: './images/slider 1.PNG'
+    },
+    {
+        name: 'MS Dhoni the untold story',
+        des: "The untold story of Mahendra Singh Dhoni's journey from ticket collector to trophy collector - the world-cup-winning captain of the Indian Cricket Team.",
+        image: './images/slider 3.PNG'
+    },
+    {
+        name: 'Toy story',
+        des:  "A cowboy doll is profoundly threatened and jealous when a new spaceman action figure supplants him as top toy in a boy's bedroom.",
+        image: './images/slider 4.PNG'
+    },
+    {
+        name: 'kaithi',
+        des:  "Dilli, an ex-convict, endeavours to meet his daughter for the first time after leaving prison. However, his attempts are interrupted due to a drug raid planned by Inspector Bejoy.",
+        image: './images/slider 5.PNG'
+    }
+]
